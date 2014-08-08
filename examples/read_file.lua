43183e742abe26258ee4b41281e10c7ed74ca864
Preston Guillory <pguillory@gmail.com>
2014-08-08 12:49:10 -0700

    Organize into directories

---
+++ examples/read_file.lua
@@ -0,0 +1,14 @@
+require 'strict'
+local ffi = require 'ffi'
+local uv = require 'uv'
+
+local this_file = debug.getinfo(1).source:sub(2)
+
+uv.run(function()
+  local file = uv.fs():open(this_file)
+  repeat
+    local chunk = file:read()
+    io.write(chunk)
+  until chunk == ''
+  file:close()
+end)
