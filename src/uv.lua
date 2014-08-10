28c8954900b6ceeb20eff97c8162fe99ce4dd3f3
Preston Guillory <pguillory@gmail.com>
2014-08-09 23:01:24 -0700

    Cleanup

--- src/uv.lua
+++ src/uv.lua
@@ -1,17 +1,14 @@
 require 'uv/cdef'
 local ffi = require 'ffi'
-local async = require 'async'
-local class = require 'class'
-local ctype = require 'ctype'
 local libuv = require 'uv/libuv'
 local libuv2 = require 'uv/libuv2'
 
 local uv_fs_t = require 'uv/uv_fs_t'
-local uv_tcp_t = require 'uv/uv_tcp_t'
-local uv_stream_t = require 'uv/uv_stream_t'
-local uv_timer_t = require 'uv/uv_timer_t'
-local uv_statbuf_t = require 'uv/uv_statbuf_t'
 local uv_loop_t = require 'uv/uv_loop_t'
+local uv_statbuf_t = require 'uv/uv_statbuf_t'
+local uv_stream_t = require 'uv/uv_stream_t'
+local uv_tcp_t = require 'uv/uv_tcp_t'
+local uv_timer_t = require 'uv/uv_timer_t'
 
 --------------------------------------------------------------------------------
 -- uv
@@ -19,16 +16,18 @@ local uv_loop_t = require 'uv/uv_loop_t'
 
 local uv = {}
 
+uv.loop = libuv.uv_default_loop()
+
 function uv.run(callback)
-  return libuv.uv_default_loop():run(callback)
+  return uv.loop:run(callback)
 end
 
 function uv.fs()
-  return libuv.uv_default_loop():fs()
+  return uv.loop:fs()
 end
 
 function uv.tcp()
-  return libuv.uv_default_loop():tcp()
+  return uv.loop:tcp()
 end
 
 return uv
