bb36ed228ad58d13ced572f2f6d8bda46cfab230
Preston Guillory <pguillory@gmail.com>
2014-08-10 17:27:33 -0700

    Use clearer file names in tests

--- test/fs_test.lua
+++ test/fs_test.lua
@@ -18,29 +18,29 @@ uv.run(function()
   assert(fs.cwd():find(dir, 1, true))
 
   -- writing
-  local file = fs.open('file', 'w', '777')
+  local file = fs.open('file.txt', 'w', '777')
   file:write('hello!')
   file:close()
 
   -- reading
-  local file = fs.open('file')
+  local file = fs.open('file.txt')
   assert(file:read() == 'hello!')
   file:fsync()
   file:close()
 
   -- hard links
-  fs.link('file', 'link')
-  local file = fs.open('link')
+  fs.link('file.txt', 'link.txt')
+  local file = fs.open('link.txt')
   assert(file:read() == 'hello!')
   file:close()
-  fs.unlink('link')
+  fs.unlink('link.txt')
 
   -- symlinks
-  fs.symlink('file', 'symlink')
-  assert(fs.readlink('symlink') == 'file')
-  fs.unlink('symlink')
+  fs.symlink('file.txt', 'symlink.txt')
+  assert(fs.readlink('symlink.txt') == 'file.txt')
+  fs.unlink('symlink.txt')
 
-  local stat = fs.stat('file')
+  local stat = fs.stat('file.txt')
   assert(stat:uid() == uid)
   -- This doesn't work! stat:gid() is returning 0 for some reason.
   -- assert(stat:gid() == gid)
@@ -51,11 +51,11 @@ uv.run(function()
   assert(math.abs(os.time() - tonumber(stat:atime())) < 10)
 
   -- renaming
-  fs.rename('file', 'new-file')
-  local file = fs.open('new-file')
+  fs.rename('file.txt', 'new-file.txt')
+  local file = fs.open('new-file.txt')
   assert(file:read() == 'hello!')
   file:close()
-  fs.unlink('new-file')
+  fs.unlink('new-file.txt')
 
   fs.rmdir(dir)
 
